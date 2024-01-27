53c3430699230049ddf6d06aa18a8c3f2d452ec6
Tengku1 <anountengku@gmail.com>
2024-01-27 19:24:53 +0700

    learn routing, css styling in next js

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import InvoicesTable from '@/app/ui/invoices/table';
+import React from 'react';
+
+export default function page() {
+  return (
+    <>
+      <p>Invoice Page</p>
+    </>
+  );
+}
